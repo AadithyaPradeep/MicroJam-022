f14643ab37c4a8281dc4ab43fc870d5f5f422334
AadithyaPradeep <aadi.prad@gmail.com>
2024-09-07 08:26:06 +0400

    PlayerMovement

---
+++ MicroJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayeMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public Vector2 moveDirection;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void Update()
+    {
+        ProcessInput();
+    }
+    public void FixedUpdate()
+    {
+        Move();
+    }
+    public void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;  
+    }
+    public void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x*moveSpeed, moveDirection.y*moveSpeed);
+    }
+}
